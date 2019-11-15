7a67f55e01a436104164dbf615c35ed46621aa55
KaitoAkita <kaitokun07@icloud.com>
2019-11-15 15:06:15 +0900

    c3.jsを用いてグラフ作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,9 @@
 class TopController < ApplicationController
   def index
+    current_username = current_user.username
+    client = K8s::Client.config(K8s::Config.load_file(File.join(Rails.root, "config", "k8s_config.yml")))
+
+    resourcequota_list = client.api('v1').resource('resourcequotas', namespace: current_user.username).list
+    @resourcequota = []
   end
 end
