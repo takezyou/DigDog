16b7e97345be2c39d5bf40f3f24bb439401feb6b
Yuto Uechi <e175706@ie.u-ryukyu.ac.jp>
2019-12-02 13:50:22 +0900

    ドメインの設定もできるようになった

--- app/controllers/domain_controller.rb
+++ app/controllers/domain_controller.rb
@@ -1,46 +1,48 @@
 class DomainController < ApplicationController
   before_action :authenticate_user!
 
-  def create
-    #デプロイの作成
+  def new
+    @domain = Domain.new()
+  end
 
-    image = "e175706/jupyter"
-    name = "testingress2"
-    port = "8888"
+  def create
+    name = domain_params[:name]
+    port = domain_params[:port]
+    svc = domain_params[:svc]
+
+    p name
+    p port
+    p svc
+
+    client = K8s::Client.config(K8s::Config.load_file(File.join(Rails.root, "config", "k8s_config.yml")))
 
     resource = K8s::Resource.new(
-      apiVersion: 'networking.k8s.io/v1beta1',
-      kind: 'Ingress',
-      metadata: {
-        name: "#{name}",
-        namespace: "e175706",
-        annotations: {
-          "nginx.ingress.kubernetes.io/rewrite-target": '/$1'
-        }
-      },
-      spec: {
-        rules: [
-          host: "#{name}.ns.ie.u-ryukyu.ac.jp",
-          http: {
-            paths: [
-              path: '/',
-              backend: {
-                serviceName: "web",
-                servicePort: 8080
-              }
+        apiVersion: 'networking.k8s.io/v1beta1',
+        kind: 'Ingress',
+        metadata: {
+            name: "#{name}",
+            namespace: current_user.username,
+            annotations: {
+                "nginx.ingress.kubernetes.io/rewrite-target": '/$1'
+            }
+        },
+        spec: {
+            rules: [
+                host: "#{name}.ns.ie.u-ryukyu.ac.jp",
+                http: {
+                    paths: [
+                        path: '/',
+                        backend: {
+                            serviceName: "#{svc}",
+                            servicePort: "#{port}".to_i
+                        }
+                    ]
+                }
             ]
-          }
-        ]
-      }
-    )
+        }
+     )
+    client.api('networking.k8s.io/v1beta1').resource('ingresses', namespace: current_user.username).create_resource(resource)
 
-    client = K8s::Client.config(
-      K8s::Config.load_file(
-        File.expand_path '~/SDN/DigDog/config/k8s_config.yml'
-      )
-    )
-
-    client.api('networking.k8s.io/v1beta1').resource('ingresses', namespace: "e175706").create_resource(resource)
   end
 
   def delete
@@ -53,5 +55,11 @@ class DomainController < ApplicationController
       render :text => e.message, :status => 500
     end
   end
+
+  private
+  def domain_params
+    params.require(:domain).permit(:name, :port, :svc)
+  end
+
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_161435) do
+ActiveRecord::Schema.define(version: 2019_11_28_175610) do
 
   create_table "creates", force: :cascade do |t|
     t.string "deploy"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_10_10_161435) do
     t.datetime "create_time"
     t.boolean "is_delete"
     t.boolean "is_recognize"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "domains", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
